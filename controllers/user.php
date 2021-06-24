e28a276130e21d72a8fb24e0c144c5221b1cd1e2
Snowy YANG <snowyyang@outlook.com>
2021-06-24 21:47:31 +0800

    edit tags

--- controllers/user.php
+++ controllers/user.php
@@ -1,20 +1,34 @@
 <?php
-if ($_REQUEST['u']) $pageuser=$db->get('users','*',['uid'=>$_REQUEST['u']]);
-else if ($url) $pageuser=$db->get('users','*',['url'=>$url]);
-if (!($model['page']=$pageuser)) {
+$columns=$_REQUEST['a']==='edittags'?['uid','name']:'*';
+if ($_REQUEST['u']) $page=$db->get('users',$columns,['uid'=>$_REQUEST['u']]);
+else if ($url) $page=$db->get('users',$columns,['url'=>$url]);
+if (!($model['page']=$page)) {
     $template='x404';
     return;
 }
-$model['works']=$db->select('works','*',['uid'=>$pageuser['uid'],'ORDER'=>'order']);
-if ($_REQUEST['a']==='edit'&&($admin||$uid===$pageuser['uid'])) {
-    if ($_POST) {
-        $result=$db->update('users',['name'=>$_POST['name'],'role'=>$_POST['role'],'contact'=>$_POST['contact'],'bio'=>$_POST['bio']],['uid'=>$uid]);
-        if ($result->rowCount()) {
-            header('Location: '.user2url($user));
-            exit;
-        }
-    }
-    $template='user_edit';
+$editable=$admin||$page['uid']===$uid;
+if ($editable&&$_REQUEST['a']==='edittags') {
+    $template='user_edittags';
+    $model['tags']=$db->select('tags','*');
+    $model['selection']=[];
+    $db->select('user_tags',['tagid'],['uid'=>$page['uid']],function($data){
+        global $model;
+        array_push($model['selection'],$data['tagid']);
+    });
 }
-else $template='user';
-$model['title']=$pageuser['name'];
+else {
+    $model['works']=$db->select('works','*',['uid'=>$page['uid'],'ORDER'=>'order']);
+    if ($editable&&$_REQUEST['a']==='edit') {
+        if ($_POST) {
+            $result=$db->update('users',['name'=>$_POST['name'],'role'=>$_POST['role'],'contact'=>$_POST['contact'],'bio'=>$_POST['bio']],['uid'=>$uid]);
+            if ($result->rowCount()) {
+                header('Location: '.user2url($user));
+                exit;
+            }
+        }
+        $template='user_edit';
+    }
+    $template='user';
+    $model['tags']=$db->select('user_tags',['[><]tags'=>['tagid'=>'tagid']],['tagname']);
+}
+$model['title']=$page['name'];
