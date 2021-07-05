c001aced145d288922adb3c1ca6f1e12d560bcaa
Snowy YANG <snowyyang@outlook.com>
2021-07-05 18:41:39 +0800

    assets, homepage

--- controllers/homepage.php
+++ controllers/homepage.php
@@ -1,5 +1,5 @@
 <?php
-$model['users']=$db->select('users',['uid','url','name','role'],['ORDER'=>['ucreatetime'=>'DESC'],'LIMIT'=>[0,10]]);
-$model['jobs']=$db->select('jobs',['jid','title'],['ORDER'=>['jtime'=>'DESC'],'LIMIT'=>[0,10]]);
+$model['users']=$db->select('users',['uid','url','name','role0','role','contact'],['ORDER'=>['ucreatetime'=>'DESC'],'LIMIT'=>[0,10]]);
+$model['jobs']=$db->select('jobs',['[><]users'=>['author'=>'uid']],['jid','title','uid','url','name','jtime'],['ORDER'=>['jcreatetime'=>'DESC'],'LIMIT'=>[0,10]]);
 $model['hottags']=$db->select('tags',['tagurl','tagname'],['hot'=>true]);
 $template='homepage';
